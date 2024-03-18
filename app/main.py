8de2c83bc4d99192852c8708709039b8cfa389ac
luciapardo <variosdemas@gmail.com>
2024-03-17 22:02:15 -0300

    feat: set db

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 import uvicorn
 from .routers import dashboard, veracity, sentiment
-from decouple import config
-
+from decouple import config, AutoConfig
+from pymongo import MongoClient
 
 app = FastAPI()
 app.include_router(dashboard.router)
@@ -10,21 +10,29 @@ app.include_router(veracity.router)
 app.include_router(sentiment.router)
 
 
-# Load environment variables from .env file
-# env_vars = dotenv_values(".env")
+@app.on_event("shutdown")
+def shutdown_db_client():
+    app.mongodb_client.close()
 
 
 def start():
-    host, port = getConfig()
+    host, port, uri, db_name = getConfig()
+    print(host, port, uri)
+    app.mongodb_client = MongoClient(uri)
+    app.database = app.mongodb_client[db_name]
     """Launched with `poetry run start` at root level"""
     uvicorn.run("app.main:app", host=host, port=port, reload=True)
 
 
+@app.on_event("startup")
 def getConfig():
     host = config("HOST")
     port = config("PORT")
-    print(host, port)
-    return host, int(port)
+    client = config("ATLAS_URI")
+    db_name = config("DB_NAME")
+
+    print(host, port, client)
+    return host, int(port), client, db_name
 
 
 @app.get("/")
