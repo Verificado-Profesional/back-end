204846ac233c594b2ef097ad1f4299c1199c0252
Robert-Iskandarani <riskandarani@fi.uba.ar>
2024-05-20 16:06:37 -0300

    Fix: se arregla ruta de tendencias

--- app/routers/tendencies.py
+++ app/routers/tendencies.py
@@ -8,7 +8,7 @@ from app.models.twitter_tendencies import TwitterTendencies
 router = APIRouter()
 
 @router.get( #TODO raise exception here
-    "/tendencies/twitter/",
+    "/trends/twitter/",
     tags=["twitter_tendencies"],
     response_description="Get twitter's tendencies from specific date ",
     response_model=List[TwitterTendencies],
@@ -17,7 +17,7 @@ async def list_tendencies(request: Request, date: str = datetime.now().strftime(
     return TendenciesController.get_twitter_tendencies(request, date, region)
 
 @router.get( #TODO raise exception here
-    "/tendencies/google/",
+    "/trends/google/",
     tags=["google_tendencies"],
     response_description="Get google's tendencies from specific date and source",
     response_model=List[GoogleTendencies],
